91bb755493e3ef98cc446c6d093bf7d465223096
Andrey Guedes <andreysgd@gmail.com>
2023-08-15 23:13:16 -0300

    getting server to work

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Produto struct {
+	Name        string
+	Description string
+	Price       float64
+	Units       int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{Name: "Dell G15", Description: "Nice hardware", Price: 4499, Units: 123},
+		{"Acer Nitro 5", "Perfect for Gamers", 5699, 57},
+		{"Alienware V13", "Behold inovation", 12899, 23},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
+}
